78e59c2cb4a0d95ddbfffc89f8601eab568edcec
D1n0sXIX <alejmaman@gmail.com>
2025-11-29 06:46:02 +0100

    Esqueleo de publciar caso

---
+++ hackednow/src/app/publicar-caso/page.jsx
@@ -0,0 +1,28 @@
+
+export default function Publicarcaso() {
+  return (
+    <div>
+      <main>
+        <h3>Publicar Tu Caso</h3>
+        <div className="Zona de texto">
+          <p>Barra input para escribir el titulo</p>
+          <p>Barra input para dar descripccion del caso</p>
+        </div>
+        <div className="Zona de multimedia">
+          <p>Div donde se pueden subir imagenes - a la derecha d</p>
+        </div>
+        <div className="Zona de categorias">
+          <p> Carrusel de categorias</p>
+          <p> Zona de input apra añadir etiquestas</p>
+        </div>
+        <div className="BotonSwitchONOFF">
+          <p>Boton Para caso Anonimo o no</p>
+        </div>
+        <p>Boton para publciar caso</p>
+        <p>Boton para guardar Borrador</p>
+
+
+      </main>
+    </div>
+  );
+}
