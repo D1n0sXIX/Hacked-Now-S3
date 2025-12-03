6c4b411476737e3387f26b8007693a7fc1a62ab6
Willyto <guillermo.rey@live.u-tad.com>
2025-12-03 09:26:58 +0100

    Update page.jsx

--- hackednow/src/app/noticias/page.jsx
+++ hackednow/src/app/noticias/page.jsx
@@ -6,7 +6,7 @@ export default function Noticias() {
     <div>
       <main className="container mx-auto px-4 py-8">
         
-        <h1 className="text-4xl font-extrabold text-white-900 mb-10">
+        <h1 className="text-4xl font-extrabold text-white mb-10">
           Ultimas Noticias
         </h1>
 
@@ -16,10 +16,11 @@ export default function Noticias() {
             
             <article 
               key={noticia.id} 
-              className="flex bg-gray-800 rounded-lg shadow-lg overflow-hidden transition-shadow duration-300 hover:shadow-xl" // Fondo azul oscuro
+              className="flex flex-col md:flex-row md:h-72 bg-gray-800 rounded-lg shadow-lg overflow-hidden transition-shadow duration-300 hover:shadow-xl"
             >
               
-              <div className="relative w-1/3 h-64 overflow-hidden flex-shrink-0">
+              {/* Bloque de la Imagen */}
+              <div className="relative w-full h-48 md:h-full md:w-1/3 overflow-hidden flex-shrink-0">
                 {/* Categoria*/}
                 <span className="absolute top-4 left-4 bg-gray-700 text-white text-xs font-semibold px-2.5 py-0.5 rounded uppercase tracking-wide z-10">
                   {noticia.categoria}
@@ -29,15 +30,15 @@ export default function Noticias() {
                 <img 
                   src={noticia.imagen} 
                   alt={noticia.titulo} 
-                  className="w-full h-full object-cover 
-                             transform transition-transform duration-300 hover:scale-110" 
+                  className="w-full h-full object-cover transform transition-transform duration-300 hover:scale-110" 
                 />
               </div>
 
-              <div className="w-2/3 p-6 flex flex-col justify-between">
+              {/* Bloque del Contenido */}
+              <div className="w-full md:w-2/3 p-6 flex flex-col justify-between md:h-full"> 
                 <div>
                   {/* Titulo de la noticia */}
-                  <h2 className="text-2xl font-bold text-white mb-4 leading-tight"> 
+                  <h2 className="text-xl md:text-2xl font-bold text-white mb-4 leading-tight"> 
                     {noticia.titulo}
                   </h2>
                   
